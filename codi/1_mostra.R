186cb7f308a72fb4d20bdb3e86c88d9a4afc629f
Ramon Puig Treserres <raypuig@gmail.com>
2022-08-22 12:27:28 +0200

    act

--- codi/1_mostra.R
+++ codi/1_mostra.R
@@ -1,24 +1,20 @@
 ##############
-#[13.08.2021]#
+#[22.08.2021]#
 ##############
 #
-#
-#
-#
 ###### lectura de dades
-
+#
+#
 link_source<-paste0("https://github.com/jrealgatius/Stat_codis/blob/master/funcions_propies.R","?raw=T")
 devtools::source_url(link_source)
-
-
+#
+#
 gc()
-
-
-generar_mostra_fitxers(directori = "dades/SIDIAP",
-                      fitxer_poblacio="HTCPANCR_entregable_poblacio_20210701_235119.rds",
-                      mida_mostra=5000,
+generar_mostra_fitxers(directori = "dades",
+                      fitxer_poblacio="BD_poblacio.rds",
+                      mida_mostra=30,
                       prefix="",
-                      directori_test="mostra2")
+                      directori_test="mostra")
 #####################################################################################
 
 
